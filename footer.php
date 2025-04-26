c579671e9090f72bdc420d662f53a1f378b87307
Safat Alnur <safat@macbookpro.home>
2025-04-26 17:09:51 -0400

    Footer update

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-        <footer>This is the footer
+        <footer id="alnur-footer">This is the footer
             <?php 
                 wp_nav_menu( array (
                     'theme_location' => 'secondary'
