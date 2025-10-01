e56f5d6c7fd771369de3299d903537fd611acd28
Safat Alnur <safat@macbookpro.home>
2025-10-01 19:22:53 -0400

    STYLE: reduce image post format feature image size

--- contents/content-image.php
+++ contents/content-image.php
@@ -1,6 +1,6 @@
 <a href="<?php echo get_permalink(); ?>"><h2>IMAGE POST FORMAT: <?php the_title(); ?></h2></a>
 
-<div class="thumb-img"><?php the_post_thumbnail('full');?></div>
+<div class="thumb-img"><?php the_post_thumbnail('medium');?></div>
 
 <p><?php the_content(); ?></p>
 
